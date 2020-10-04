93f553e63a02aa546d168d0d2f50acf1e0c512ea
Nabil Babu <nabil.babu20@gmail.com>
2020-10-04 18:29:13 -0400

    Initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public GameObject EnemySpawner;
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    public TMP_Text sceneLabel; 
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel; 
+   
+
+    // Event Handler for the StartButton_Pressed Event
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        
+    }
+}
